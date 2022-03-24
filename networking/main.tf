075db1d085318cd3d297f9476d090599c8cb60bd
Antonio Tarifa <antonio.tarifa.sanchez@outlook.com>
2022-03-24 17:12:24 +0000

    Lesson 89

--- networking/main.tf
+++ networking/main.tf
@@ -127,9 +127,9 @@ resource "aws_security_group" "mtc_sg" {
 }
 
 
-resource "aws_db_subnet_group" "mtc_rds_sg" {
-  count = var.db_subnet_group == true ? 1 : 0
-  name = "mtc_rds_subnetgroup"
+resource "aws_db_subnet_group" "mtc_rds_subnetgroup" {
+  count      = var.db_subnet_group == true ? 1 : 0
+  name       = "mtc_rds_subnetgroup"
   subnet_ids = aws_subnet.mtc_private_subnet.*.id
   tags = {
     Name = "mtc_rds_sgn"
